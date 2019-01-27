7d87e732c5a19756e8b14289afddcc14bffc2a9e
kajopelayeola@gmail <okajopel@cisco.com>
2019-01-27 15:32:12 -0500

    Register tooltip with parent component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
+  toolTip: Object = {};
+  private textForToolTipA: String = 'Hey! I\'m tool tip A. Guess what? A is the 1st letter in the alphabet';
+  private textForToolTipB: String = 'Hey! I\'m tool tip B. And B is the 2nd letter in the alphabet'
+
+  showToolTip() {
+    this.toolTip = { show: true };
+  }
 }
